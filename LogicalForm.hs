8637b7d2fea23d5b737eeba10b291bcb53da1c27
Dr Bean <drbean@localhost.localdomain>
2014-04-05 22:20:59 +0800

    GPosQ,GNegQ-GYN-GSentence:keep_together

--- LogicalForm.hs
+++ LogicalForm.hs
@@ -97,9 +97,9 @@ transS ((GUt (GPosQ (GWH_Pred wh vp)))) =
 transS ((GUt (GNegQ (GWH_Pred wh vp)))) =
 	WH (\x -> Conj [ transW wh x, Neg (transVP vp x)])
 transS (GUt (GPosQ (GYN (GSentence np vp)))) = (transNP np) (transVP vp)
+transS (GUt (GNegQ (GYN (GSentence np vp)))) = (transNP np) (transVP vp)
 transS (GUt (GPosQ (GYN (GFeel_helpless np)))) = transNP np (\e -> Rel "helpless" [e])
 transS (GUt (GPosQ (GYN (GFeel_unsupported np)))) = transNP np (\e -> Rel "unsupported" [e])
-transS (GUt (GNegQ (GYN (GSentence np vp)))) = (transNP np) (transVP vp)
 transS (GUt (GPosQ (GTagQ np vp))) = (transNP np) (transVP vp)
 transS (GUt (GNegQ (GTagQ np vp))) = (transNP np) (transVP vp)
 transS (GUt (GPosQ (GTagComp np comp))) = (transNP np) (transCOMP comp)
