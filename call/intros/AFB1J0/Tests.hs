bbb3a950c7d0904aaa794b77bdb19bf2fb47b061
Dr Bean <drbean@localhost.localdomain>
2014-04-14 22:40:47 +0800

    follow_stress/cusp/Tests.move_handler

--- call/intros/AFB1J0/Tests.hs
+++ call/intros/AFB1J0/Tests.hs
@@ -1,78 +1,8 @@
 module Tests where
 
-
 import Control.Monad
 
-import PGF
-import AFB1J0
-import LogicalForm
-import Evaluation
-import Model
-import WordsCharacters
-
-import Data.List
-import Data.Char
-
--- handler gr core tests = putStr $ unlines $ map (\(x,y) -> x++show y) $ zip (map (++"\t") tests ) ( map (\string -> map (\x -> core ( x) ) (parse gr (mkCId "DicksonEng") (startCat gr) string)) tests )
-
--- import System.Environment.FindBin
-
-ans tests = do
-  gr	<- readPGF ( "./AFB1J0.pgf" )
-  let ss = map (chomp . lc_first) tests
-  let ps = map ( parses gr ) ss
-  let ls = map ( map ( linear transform gr ) ) ps
-  let zs = zip (map (++"\t") tests) ls
-  putStrLn (unlines (map (\(x,y) -> x ++ (show $ concat y)) zs) )
-
---trans tests = do
---  gr	<- readPGF ( "./Dickson.pgf" )
---  let ss = map (chomp . lc_first) tests
---  let ps = map ( parses gr ) ss
---  let ls = map id ps
---  let zs = zip (map (++"\t") tests) ps
---  putStrLn (unlines (map (\(x,y) -> x ++ (show (concat y ) ) ) zs) )
-
-logic tests = do
-  gr	<- readPGF ( "./AFB1J0.pgf" )
-  let ss = map (chomp . lc_first) tests
-  let ps = map ( parses gr ) ss
-  let ts = map (map (map lf)) ps
-  let zs = zip (map (++"\t") tests) ts
-  putStrLn (unlines (map (\(x,y) -> x ++ (show $ concat y ) ) zs) )
-
-parses :: PGF -> String -> [[Tree]]
-parses gr s = ( parseAll gr (startCat gr) s )
-
-transform :: Tree -> Tree
-transform = gf . answer . fg
-
-lf :: Tree -> LF
-lf =  transS . fg
-
-answer :: GUtt -> GUtt
--- answer (GUt (GPosQ (GWH_Cop Gwho_WH np)))	= (GUt (GNegQ (GTagQ np (GHappening Glaugh))))
--- answer (GUt (GPosQ (GWH_Pred Gwho_WH (GChanging v np)))) = (GUt (GNegQ (GTagQ np (GHappening Glaugh))))
--- answer (GUt (GPosQ (GWH_Pred Gwho_WH (GHappening vp)))) = Gdee
--- answer (GUt (GPosQ (GYN (GCop np1 np2))))  = np1
-answer utt	| (eval . transS) utt == Boolean True = GYes
-		| (eval . transS) utt == Boolean False = GNo
-		| (eval . transS) utt == NoAnswer = GNoAnswer
-
-linear :: (Tree -> Tree) -> PGF -> [Tree] -> [ String ]
-linear tr gr ps = concat $ map ((linearizeAll gr) . tr) ps
-
-lc_first :: String -> String
-lc_first str@(s:ss) = case ( or $ map (flip isPrefixOf str) ["Alf", "Dee"] ) of
-	True  -> (s:ss)
-	False -> ((toLower s):ss)
-
-chomp :: String -> String
-chomp []                      = []
--- chomp ('\'':'s':xs)           = " 's" ++ chomp xs
--- chomp ('s':'\'':xs)           = "s 's" ++ chomp xs
-chomp (x:xs) | x `elem` ".,?" = chomp xs
-            | otherwise      =     x:chomp xs
+import Handler
 
 roger_test = [
 
