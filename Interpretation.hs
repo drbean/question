9ca2b21bc02345597db09f322593aa7e69ea9e17
Dr Bean <drbean@localhost.localdomain>
2014-08-20 12:32:19 +0900

    Works--int=evalu::Maybe_[Entity]→Boool→[Entity]→Bool,lookIntuples::String→Maybe_[Entity]→Bool,uninflect::String→Maybe_String:unmay1,unmay2,unmay3,unmay4.comment_out:args,forgetful

--- Interpretation.hs
+++ Interpretation.hs
@@ -1,6 +1,8 @@
 module Interpretation (module Interpretation, module Topic, module Story_Interpretation) where 
 
 import Data.List
+import Data.Maybe
+import Control.Monad
 
 import Model
 import qualified Topic_Interpretation as Topic
@@ -8,27 +10,34 @@ import Story_Interpretation
 
 type Interp a	= String -> [a] -> Bool
 
+inttuples :: [(String, [Entity] -> Bool)]
 inttuples = common_objects ++ common_relations ++ objects ++ relations
 			    ++ Topic.objects ++ Topic.relations
+
+infltuples :: [(String, String)]
 infltuples = common_inflections ++ Topic.inflections ++ inflections
 
 int :: Interp Entity
-int = lookIntuples . uninflect
+int r ents = evalu ((lookIntuples <=< uninflect) r) ents
 
-lookIntuples :: String -> [Entity] -> Bool
-lookIntuples word	| Just interpretation <- lookup word inttuples = interpretation
-					| otherwise = \_ -> error ( "'" ++ word ++ "'" ++ " has no interpretation" )
+evalu :: Maybe ([Entity] -> Bool ) -> [Entity] -> Bool
+evalu p ints = (fromMaybe (\_ -> False) p) ints
 
-uninflect :: String -> String
-uninflect word	| Just rel <- lookup word infltuples = rel
-								| otherwise = word
+lookIntuples :: String -> Maybe ( [Entity] -> Bool )
+lookIntuples word	| Just interpretation <- lookup word inttuples = Just interpretation
+					| otherwise = Nothing
+
+
+uninflect :: String -> Maybe String
+uninflect word	| Just rel <- lookup word infltuples = Just rel
+								| otherwise = Just word
 
 common_objects, common_relations :: [( String, [Entity] -> Bool)]
 common_objects = [
-	( "person",	\ [x] -> person x	)
-	, ( "thing",	\ [x] -> thing x	)
-	, ( "man",	\ [x] -> predid1 "male" x	)
-	, ( "woman",	\ [x] -> predid1 "female" x	)
+	( "person",	\[x] -> person x )
+	, ( "thing",	\[x] -> thing x )
+	, ( "man", \[x] -> 	unmay1 (predid1 "male") x )
+	, ( "woman",	\[x] -> unmay1 (predid1 "female") x )
  ]
 
 common_inflections :: [(String, String)]
@@ -61,9 +70,9 @@ common_inflections = [
 
 common_relations = [
 
- ( "true", \[x] -> predid1 "true" x )
- , ( "had", \[x,y] -> predid2 "have" y x )
- , ( "gave",	\ [x,y,z] ->	predid3 "gave" z y x )
+ ( "true", \[x] -> unmay1 (predid1 "true") x )
+ , ( "had", \[x,y] -> unmay2 (predid2 "have") y x )
+ , ( "gave",	\ [x,y,z] ->	unmay3 (predid3 "gave") z y x )
  --( "work", \args -> case args of
  --       [x] -> worker x
  --       [x,y] -> predid2 "work_where" y x || predid2 "work_as" y x ),
@@ -71,9 +80,9 @@ common_relations = [
 --	[x,y,z,w] -> wanted w z y x
 --	[x,y,z] -> forgetful4 wanted z y x
 --	[x,y]	-> (forgetful3 . forgetful4 ) wanted y x ),
- , ( "got", \args -> case args of
- 	[x,y,z] -> predid3 "got" z y x
- 	[x,y] -> forgetful3 (predid3 "got") y x )
+ --, ( "got", \args -> case args of
+ --	[x,y,z] -> predid3 "got" z y x
+ --	[x,y] -> forgetful3 (predid3 "got") y x )
  ]
 
 -- vim: set ts=2 sts=2 sw=2 noet:
