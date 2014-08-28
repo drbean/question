48a48424c01a86fc4af1c529c83fc5d9fb1f6933
Dr Bean <drbean@localhost.localdomain>
2014-08-28 14:45:25 +0900

    post-Dickson_gfWords.0,_,zero_determiner.haskell_after.vim.to:adv,prep

--- business/careers/candidate/WordsCharacters.hs
+++ business/careers/candidate/WordsCharacters.hs
@@ -1,19 +1,20 @@
-module WordsCharacters where 
+module WordsCharacters where
 
 import qualified Data.Map as Map
 
-gfWords = Map.fromList [("A",a),
-	-- ("ADV",adv),
-	("Aux",aux),
-	-- ("CONJ",conj),
-	("Det",det),
-	("N",n),
-	("PN",pn),
-	-- ("Pron",pron),
-	("Prep",prep),
-	-- ("Rel",rel),
-	("Tag",tag),
-	("V",v) ]
+gfWords = Map.fromList [
+	("A",a)
+	, ("ADV",adv)
+	, ("Aux",aux)
+	, ("CONJ",conj)
+	, ("Det",det)
+	, ("N",n)
+	, ("PN",pn)
+	, ("Pron",pron)
+	, ("Prep",prep)
+	, ("Rel",rel)
+	, ("Tag",tag)
+	, ("V",v) ]
 
 wordlist = concat ( map (gfWords Map.!) (Map.keys gfWords) )
 
@@ -33,60 +34,31 @@ posMap = Map.fromList [
 	]
 
 a = [
-	"ambitious"
+	"Polish"
+	, "ambitious"
 	, "bad"
 	, "competitive"
 	, "confident"
 	, "difficult"
-	, "each"
-	, "experienced"
 	, "fast"
-	, "few"
-	, "five"
 	, "good"
 	, "hard"
-	, "honest"
-	, "local"
-	, "long"
+	, "main"
 	, "next"
-	, "other"
 	, "outgoing"
-	, "past"
 	, "patient"
-	, "Polish"
-	, "poor"
 	, "possible"
 	, "realistic"
-	, "safe"
-	, "soon"
 	, "successful"
 	, "true"
-	, "unable"
 	, "various"
-
 
 	]
 
 adv = [
-	"along"
-	, "away"
-	, "because"
-	, "before"
-	, "but"
-	, "differently"
-	, "down"
-	, "ever"
-	, "finally"
-	, "here"
-	, "just"
-	, "now"
-	, "on"
-	, "out"
-	, "probably"
-	, "so"
-	, "then"
-	, "there"
-	, "too"
+	"really"
+	, "slowly"
+	, "soon"
 	, "well"
 	]
 
@@ -103,63 +75,53 @@ aux = [
 	
 
 conj = [
-	"and"
-	, "if"
+	"because"
 	]
 
 
 det = [
-	"a"
+	"'s"
+	, "0, _ or zero"
+	, "a"
 	, "an"
 	, "no"
 	, "some"
-	-- , "that"
-	-- , "theirs"
-	-- , "these"
 	, "the"
+	, "five"
+	, "a few"
+	, "any"
+	, "many"
+	, "a lot of"
 	]
 
 n = [
+
 	"ability"
 	, "administration"
 	, "aim"
-	, "business"
-	, "club"
 	, "company"
-	, "course"
 	, "department"
 	, "director"
-	, "director_2"
-	, "effort"
-	, "experience"
-	, "hand"
 	, "head"
 	, "job"
 	, "judgement"
 	, "learner"
-	, "lot"
-	, "main"
+	, "local business club"
 	, "market"
+	, "market share"
 	, "material"
-	, "pair"
 	, "people"
 	, "personality"
-	, "polish_N"
-	, "respect"
+	, "Polish"
 	, "result"
 	, "risk"
-	, "sales department"
+	, "safe pair of hands"
 	, "sales experience"
-	, "share"
-	, "system"
-	, "team"
-	, "term"
+	, "sales team"
 	, "thing"
 	, "time"
 	, "top"
-	, "training"
-	, "way"
-	, "work"
+	, "training course"
 	, "year"
 
 	]
@@ -168,79 +130,204 @@ pn = [
 	"Barbara"
 	, "Tadeusz"
 	, "Eva"
+	, "Dr Bean"
 	, "Fast-Track"
 	]
 
 pron = [
-	"he"
-	, "i"
-	, "i'm"
-	, "it"
-	, "it's"
-	, "me"
-	, "myself"
-	, "they"
-	, "us"
-	, "we"
-	, "you"
-	, "you're"
-	, "who"
+	"who"
+	, "whom"
 	]
 
 prep = [
-	"of"
-	-- , "about"
-	-- , "at"
-	-- , "for"
-	-- , "from"
-	-- , "like"
+	"for"
 	, "in"
-	-- , "than"
-	-- , "to"
-	-- , "with"
+	, "of"
+	, "over"
+	, "to"
+	, "with"
 	]
 
 rel = [
 	"that"
+	, "who"
 	]
 
 tag = [
-	"doesn't he,"
-	, "doesn't she,"
-	, "doesn't it,"
-	, "don't they,"
-	, "does he,"
-	, "does she,"
-	, "does it,"
-	, "do they,"
+	"doesn't he"
+	, "doesn't she"
+	, "doesn't it"
+	, "don't they"
+	, "does he"
+	, "does she"
+	, "does it"
+	, "do they"
+	, "isn't he"
+	, "isn't she"
+	, "isn't it"
+	, "aren't they"
+	, "is he"
+	, "is she"
+	, "is it"
+	, "are they"
 	]
 
 v = [
 
 	"become"
 	, "can"
-	, "get"
-	, "get_along"
-	, "go"
-	, "have"
-	, "hire"
-	, "interview"
 	, "know"
-	, "laugh"
-	, "lift"
+
+	, "apply"
+	, "choose"
+	, "enjoy"
+	, "expand"
+	, "feel"
+	, "get to"
+	, "get"
+	, "have"
+	, "improve"
+	, "increase"
 	, "like"
-	, "look_here"
-	, "look"
-	, "need"
-	, "prove"
+	, "motivate"
+	, "offer"
+	, "organize"
+	, "respect"
 	, "say"
-	, "see"
-	, "slow_down"
 	, "start"
 	, "take"
 	, "tell"
 	, "think"
+	, "try"
+	, "want"
+	, "win"
 	, "work"
-
 	]
 
+{-
+
+Barbara	: PN;
+drbean	: PN;
+Eva	: PN;
+Fast-Track	: PN;
+Polish : A, PN;
+Tadeusz	: PN;
+ability	: NP;
+administration	: NP;
+aim	: CN;
+ambitious	: A;
+any	: Det;
+apply	: V;
+as	: Prep;
+bad	: A;
+because	: Subj;
+better	: A;
+by	: Prep;
+can	: V;
+choose	: V;
+local business club	: CN;
+company	: CN;
+competitive	: A;
+confident	: A;
+department	: CN;
+difficult	: A;
+director	: CN;
+each
+enjoy	: V;
+expand	: V;
+experience	: NP;
+fast	: A;
+feel	: V;
+a few	: Det;
+five	: Det;
+for	: Prep;
+get	: V;
+good	: A;
+
+hard	: A;
+have	: V;
+he
+head	: CN;
+help
+if
+improve	: V;
+in	: Prep;
+increase	: V;
+is
+it
+job	: CN;
+judgement	: NP;
+know
+lead
+learner	: CN;
+like	: V;
+a lot of	: Det;
+main	: A;
+many	: Det;
+market	: CN;
+market share	: CN;
+material	: NP;
+may
+maybe
+motivate	: V;
+much
+next	: A;
+of
+offer	: V;
+on
+organize	: V;
+other
+outgoing	: A;
+over	: Prep;
+patient	: A;
+people	: CN;
+personality	: CN;
+possible	: A;
+realistic	: A;
+really	: Adv;
+respect	: V;
+result	: CN;
+risk	: CN;
+safe pair of hands	: CN;
+sales experience	: NP;
+sales team	: CN;
+say	: V;
+see
+selling
+set
+she
+should
+slowly	: Adv;
+soon	: Adv;
+start	: V;
+successful	: A;
+systems
+take	: V;
+tell	: V;
+"in the long-term"
+that
+the
+thing	: CN;
+think	: V;
+time	: NP;
+to	: Prep;
+too
+top	: CN;
+training course	: CN;
+try	: V;
+true	: A;
+unable
+up
+various	: A;
+very
+want	: V;
+well
+what
+win	: V;
+with	: Prep;
+work	: V;
+year	: CN;
+
+-}
+
+-- vim: set ts=2 sts=2 sw=2 noet:
