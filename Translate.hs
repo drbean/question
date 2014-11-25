ae3f0cfd0e1e9d2e40df3dd30ad8aa936b9d341f
Dr Bean <drbean@localhost.localdomain>
2014-11-25 08:59:08 +0800

    ported_Data.DRS.Translate_removing_drsRelToString,drsRefToDRSVar,Prop,Diamond,Box,comments.FOLForm^LF

---
+++ Translate.hs
@@ -0,0 +1,30 @@
+module Translate
+(
+  drsToLF
+) where
+
+import Data.DRS.DataType
+import Data.DRS.Variables
+import qualified LogicalForm as L
+
+drsToLF :: DRS -> L.LF
+drsToLF (LambdaDRS _)  = error "infelicitous LF formula"
+drsToLF (Merge _ _)    = error "infelicitous LF formula"
+drsToLF (DRS _ cs) = drsConsToLF cs
+-- drsToLF (DRS _ cs) (r:rs) = (L.Exists r (drsToLF (DRS rs cs)) ) rs
+
+drsConsToLF :: [DRSCon] -> L.LF
+drsConsToLF [] = L.Top
+drsConsToLF (Rel r d:[]) = L.Rel r d
+drsConsToLF (Neg d1:[]) = L.Neg (drsToLF d1)
+drsConsToLF (Imp d1 d2:[]) = quantifyForAll d1 where
+	quantifyForAll :: DRS -> L.LF
+	quantifyForAll (LambdaDRS _)  = error "infelicitous LF formula"
+	quantifyForAll (Merge _ _)    = error "infelicitous LF formula"
+	quantifyForAll (DRS [] c)     = L.Imp    (drsConsToLF c) (drsToLF d2)
+	-- quantifyForAll (DRS (r:rs) c) = L.ForAll (drsRefToDRSVar r)  (quantifyForAll (DRS rs c))
+drsConsToLF (Or d1 d2:[]) = L.Or (drsToLF d1) (drsToLF d2)
+-- drsConsToLF (Prop r d1:[]) = (drsToLF d1 (drsRefToDRSVar r))
+drsConsToLF (Diamond d1:[]) = error "infelicitous LF formula"
+drsConsToLF (Box d1:[]) = error "infelicitous LF formula"
+drsConsToLF (c:cs) = L.And (drsConsToLF [c]) (drsConsToLF cs)
