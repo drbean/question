5acb48892db2692bcb67a0fcf7c7a9c18997443b
Dr Bean <drbean@localhost.localdomain>
2014-11-28 14:38:49 +0800

    out:resolveDrsToLF.back_in:drsToLF::DRSUnresolved→[DRSRef]→L.LF.case_of_Rel_r_d.mkLF_Rel=Rel_r_rs

--- Translate.hs
+++ Translate.hs
@@ -1,6 +1,6 @@
 module Translate
 (
-  resolveDrsToLF
+  drsToLF
 ) where
 
 import Data.DRS.DataType
@@ -9,26 +9,10 @@ import qualified LogicalForm as L
 
 type DRSUnresolved = [DRSRef] -> DRS
 
-resolveDrsToLF :: DRSUnresolved -> [DRSRef] -> L.LF
-resolveDrsToLF ud rs | (LambdaDRS _) <- (ud rs) = error "infelicitous LF formula"
-resolveDrsToLF ud rs | (Merge _  _) <- (ud rs) = error "infelicitous LF formula"
-resolveDrsToLF ud rs | (DRS _ cs) <- (ud rs) = L.Exists ((drsConsToLF cs) rs)
+drsToLF :: DRSUnresolved -> ([DRSRef] -> L.LF)
+drsToLF ud rs = case (ud rs) of
+	(DRS _ (Rel r d : [])) -> (\rs' -> (mkLF rs' (L.Rel r rs') )) d
+	(DRS _ _) -> (\rs' -> (mkLF rs' (L.Rel (DRSRel "queen") rs') )) [DRSRef "x"]
 
-drsToLF :: DRS -> L.LF
-drsToLF (DRS _ cs) = drsConsToLF cs
-
-drsConsToLF :: [DRSCon] -> L.LF
-drsConsToLF [] = L.Top
-drsConsToLF (Rel r d : []) = L.Rel r d
-drsConsToLF (Neg d1 : []) = L.Neg (drsToLF d1)
-drsConsToLF (Imp d1 d2 : []) = quantifyForAll d1 where
-	quantifyForAll :: DRS -> L.LF
-	quantifyForAll (LambdaDRS _)  = error "infelicitous LF formula"
-	quantifyForAll (Merge _ _)    = error "infelicitous LF formula"
-	quantifyForAll (DRS _ c)     = L.Imp (drsConsToLF c) (drsToLF d2)
-	-- quantifyForAll (DRS (r:rs) c) = L.ForAll (drsRefToDRSVar r)  (quantifyForAll (DRS rs c))
-drsConsToLF (Or d1 d2 : []) = L.Or (drsToLF d1) (drsToLF d2)
-drsConsToLF (Prop r d : []) = drsToLF d
-drsConsToLF (Diamond d1 : []) = error "infelicitous LF formula"
-drsConsToLF (Box d1 : []) = error "infelicitous LF formula"
-drsConsToLF (c:cs) = L.And (drsConsToLF [c]) (drsConsToLF cs)
+mkLF :: [DRSRef] -> L.LF -> L.LF
+mkLF _ (L.Rel r rs) = L.Rel r rs
