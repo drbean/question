e178c48a029ad968303f430075b57c6d3b8bd5e0
Dr Bean <drbean@localhost.localdomain>
2014-11-29 16:42:34 +0800

    drsToLF:case_ud_rs_of_LambdaDRS,Merge:error.DRS_rs:Exists_rs

--- Translate.hs
+++ Translate.hs
@@ -11,8 +11,11 @@ type DRSUnresolved = [DRSRef] -> DRS
 
 drsToLF :: DRSUnresolved -> ([DRSRef] -> L.LF)
 drsToLF ud rs = case (ud rs) of
+	(LambdaDRS _) -> error "infelicitous FOL formula"
+	(Merge _ _) -> error "infelicitous FOL formula"
+	(DRS _ []) -> (\rs' -> L.Top ) rs
 	(DRS _ (Rel name rs' : [])) -> (\rs'' -> (L.Rel name rs'') ) rs'
-	(DRS _ (Rel name rs' : cs)) -> (\rs'' -> L.And (L.Rel name rs') (drsToLF ( \rs'' -> DRS rs'' cs) rs) ) rs
+	(DRS _ (Rel name rs' : cs)) -> L.Exists (\rs'' -> L.And (L.Rel name rs') (drsToLF ( \rs'' -> DRS rs'' cs) rs) )
 	(DRS _ _) -> (\rs' -> (mkLF rs' (L.Rel (DRSRel "queen") rs') )) [DRSRef "x"]
 
 mkLF :: [DRSRef] -> L.LF -> L.LF
