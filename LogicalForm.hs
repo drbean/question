b74262440b38fdc2af5fab45a765a75f359ab5a9
Dr Bean <drbean@localhost.localdomain>
2014-11-29 12:13:15 +0800

    hiding:Or,Imp,Neg,Rel.DRS.FOL.Show:showFOLForm^showLForm.aargh:haskell_indent_formatting.unicode_logical_symbols

--- LogicalForm.hs
+++ LogicalForm.hs
@@ -1,6 +1,8 @@
 module LogicalForm (module LogicalForm) where
 
-import Data.DRS.DataType hiding (Rel)
+import Data.DRS.DataType hiding (Or,Imp,Neg,Rel)
+import Data.DRS.Variables
+import Data.List
 
 data LF = NonProposition
 	| ForAll ([DRSRef] -> LF)
@@ -14,12 +16,59 @@ data LF = NonProposition
 	| Bottom
 --	deriving (Eq)
 
+
 instance Show LF where
-	show (Rel r rs) = show r ++ " " ++ show rs
+  show = showLForm
+
+showLForm :: LF -> String
+showLForm f = '\n' : showFormula f ++ "\n"
+  where showFormula :: LF -> String
+        -- showFormula (Exists v f1) = opExists ++ v ++ showFormula f1
+        -- showFormula (ForAll v f1) = opForAll ++ v ++ showFormula f1
+        showFormula (And f1 f2)   = "(" ++ showFormula f1 ++ " "  ++ opAnd ++ " "  ++ showFormula f2 ++ ")"
+        showFormula (Or f1 f2)    = "(" ++ showFormula f1 ++ ") " ++ opOr  ++ " (" ++ showFormula f2 ++ ")"
+        showFormula (Imp f1 f2)   = "(" ++ showFormula f1 ++ ") " ++ opImp ++ " (" ++ showFormula f2 ++ ")"
+        showFormula (Neg f1)      = opNeg ++ showFormula f1
+        showFormula (Rel r d)     = (drsRelToString r) ++ "(" ++ intercalate "," (map drsRefToDRSVar d) ++ ")"
+        showFormula (Top)         = opTop
+        showFormula (Bottom)      = opBottom
+
 
 relname :: LF -> String
 relname (Rel (DRSRel name) _) = name
 -- relname lf = error ( (show lf) ++ " not a relation" )
 relname lf = error ( "The formula is " ++ " not a relation" )
+
+-- | Existential quantifier symbol
+opExists :: String
+opExists = "\x2203"
+
+-- | Universal quantifier symbol
+opForAll :: String
+opForAll = "\x2200"
+
+-- | Conjunction symbol
+opAnd :: String
+opAnd    = "\x2227"
+
+-- | Disjunction symbol
+opOr :: String
+opOr     = "\x2228"
+
+-- | Implication symbol
+opImp :: String
+opImp    = "\x2192"
+
+-- | Negation symbol
+opNeg :: String
+opNeg    = "\x00AC"
+
+-- | Top/true constant symbol
+opTop:: String
+opTop    = "\x22A4"
+
+-- | Bottom/false constant symbol
+opBottom :: String
+opBottom = "\x22A5"
 
 -- vim: set ts=2 sts=2 sw=2 noet:
