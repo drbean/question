39c2322f34fdb8d31839db35b33ebd7fefc85c29
Dr Bean <drbean@localhost.localdomain>
2014-11-30 15:03:41 +0800

    drsToLF:Neg_d.drsConsToLF:Neg_d.no_parentheses_around_case_uc_rs_of_single-element_list

--- Translate.hs
+++ Translate.hs
@@ -15,10 +15,12 @@ drsToLF ud rs = case (ud rs) of
 	(Merge _ _) -> error "infelicitous FOL formula"
 	(DRS _ []) -> (\rs' -> L.Top ) rs
 	(DRS _ (Rel name rs' : cs)) -> L.Exists (\rs'' -> L.And (L.Rel name rs') (drsConsToLF ( \rs'' -> cs) rs) )
+	(DRS _ [Neg d]) -> (\rs' -> L.Neg (drsToLF (\rs'' -> d) rs') ) rs
 
 drsConsToLF :: ([DRSRef] -> [DRSCon]) -> ([DRSRef] -> L.LF)
 drsConsToLF uc rs = case (uc rs) of
-	([]) -> L.Top
-	([Rel r rs']) -> L.Rel r rs'
-	(Prop r d: []) -> L.And (L.Rel (DRSRel (drsRefToDRSVar r)) rs) (drsToLF (\rs' -> d) rs)
+	[] -> L.Top
+	[Rel r rs'] -> L.Rel r rs'
+	[Neg d] -> L.Neg (drsToLF (\rs' -> d) rs)
+	[Prop r d] -> L.And (L.Rel (DRSRel (drsRefToDRSVar r)) rs) (drsToLF (\rs' -> d) rs)
 	(c:cs) -> L.And (drsConsToLF (\rs'' -> [c]) rs) (drsConsToLF (\rs'' -> cs) rs)
