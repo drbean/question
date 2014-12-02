3ee5aa7868b57b88d1c6ba30198a9d00c96ea901
Dr Bean <drbean@localhost.localdomain>
2014-12-02 17:06:43 +0800

    remove_DRSRef_from_LF_reinstate_commit_bf428397455_and_before:Term,but_retaining_Top,Bottom,[Term]_as_list.combinatorial_explosion_not_affected.import_Model.Entity

--- LogicalForm.hs
+++ LogicalForm.hs
@@ -4,18 +4,34 @@ import Data.DRS.DataType hiding (Or,Imp,Neg,Rel)
 import Data.DRS.Variables
 import Data.List
 
+import Model
+
+data Term = Const Entity | Var String | Struct String [Term]
+        deriving (Eq)
+
 data LF = NonProposition
-	| ForAll ([DRSRef] -> LF)
-	| Exists ([DRSRef] -> LF)
-	| And LF LF
-	| Or LF LF
-	| Imp LF LF
-	| Neg LF
-	| Rel DRSRel [DRSRef]
 	| Top
 	| Bottom
---	deriving (Eq)
+	| Rel String [Term]
+	| Neg  LF
+	| Imp LF LF
+	| And LF LF
+	| Or LF LF
+	| Forall ([Term] -> LF)
+	| Exists ([Term] -> LF)
+	| Single ([Term] -> LF)
+	| The ([Term] -> LF)
+	| Several ([Term] -> LF)
+	| Many ([Term] -> LF)
+	| Most ([Term] -> LF)
+	| WH ([Term] -> LF)
+	--      deriving Eq
 
+instance Show Term where
+  show (Const name) = show name
+  show (Var i)      = 'x': show i
+  show (Struct s []) = s
+  show (Struct s ts) = s ++ show ts
 
 instance Show LF where
   show = showLForm
@@ -23,20 +39,20 @@ instance Show LF where
 showLForm :: LF -> String
 showLForm f = '\n' : showFormula f ++ "\n"
   where showFormula :: LF -> String
-        showFormula (Exists udrs) = opExists ++ "xyzw" ++ showFormula (udrs 
-					[DRSRef "x", DRSRef "y", DRSRef "z", DRSRef "w"])
+        showFormula (Exists udrs) = opExists ++ "xyzw" ++ showFormula (udrs
+					[Var "x", Var "y", Var "z", Var "w"])
         -- showFormula (ForAll v f1) = opForAll ++ v ++ showFormula f1
         showFormula (And f1 f2)   = "(" ++ showFormula f1 ++ " "  ++ opAnd ++ " "  ++ showFormula f2 ++ ")"
         showFormula (Or f1 f2)    = "(" ++ showFormula f1 ++ ") " ++ opOr  ++ " (" ++ showFormula f2 ++ ")"
         showFormula (Imp f1 f2)   = "(" ++ showFormula f1 ++ ") " ++ opImp ++ " (" ++ showFormula f2 ++ ")"
         showFormula (Neg f1)      = opNeg ++ showFormula f1
-        showFormula (Rel r d)     = (drsRelToString r) ++ "(" ++ intercalate "," (map drsRefToDRSVar d) ++ ")"
+        showFormula (Rel r d)     = r ++ "(" ++ intercalate "," (map show d) ++ ")"
         showFormula (Top)         = opTop
         showFormula (Bottom)      = opBottom
 
 
 relname :: LF -> String
-relname (Rel (DRSRel name) _) = name
+relname (Rel name _) = name
 -- relname lf = error ( (show lf) ++ " not a relation" )
 relname lf = error ( "The formula is " ++ " not a relation" )
 
