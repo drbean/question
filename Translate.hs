060634a966009a780e2cb91bfc9acfe793e32e13
Dr Bean <drbean@localhost.localdomain>
2014-12-13 12:48:50 +0800

    drsToLF::DRSUnresolved_[DRSRef^Term]_LF.case_term2ref_ts_of_singleton[r]:Exists

--- Translate.hs
+++ Translate.hs
@@ -9,17 +9,23 @@ import qualified LogicalForm as L
 
 type DRSUnresolved = [DRSRef] -> DRS
 
-drsToLF :: DRSUnresolved -> ([DRSRef] -> L.LF)
-drsToLF ud rs = case (ud rs) of
-	(LambdaDRS _) -> error "infelicitous FOL formula"
-	(Merge _ _) -> error "infelicitous FOL formula"
-	(DRS _ []) -> (\rs' -> L.Top ) rs
-	(DRS _ (Rel (DRSRel name) rs' : cs)) -> case rs' of
-			[r] -> L.Exists (\t -> L.Conj [ (L.Rel name [t]) ,
-				(drsConsToLF ( \rs'' -> cs) rs) ])
-			_ -> L.Conj [ (L.Rel name (map L.Var rs')),
-				(drsConsToLF ( \rs'' -> cs) rs) ]
-	(DRS _ [Neg d]) -> (\rs' -> L.Neg (drsToLF (\rs'' -> d) rs') ) rs
+term2ref :: L.Term -> DRSRef
+term2ref (L.Var r) = r
+term2ref _ = undefined
+
+drsToLF :: DRSUnresolved -> ([L.Term] -> L.LF)
+drsToLF ud ts = case (map term2ref ts) of
+	[r] -> case (ud [r]) of
+		(LambdaDRS _) -> error "infelicitous FOL formula"
+		(Merge _ _) -> error "infelicitous FOL formula"
+		(DRS _ []) -> (\t -> L.Top ) (L.Var r)
+		(DRS _ (Rel (DRSRel name) r' : cs)) -> case r' of
+			[r''] -> L.Exists (\t -> L.Conj [ (L.Rel name [t]) ,
+				(drsConsToLF ( \rs'' -> cs) [r]) ])
+			_ -> L.Conj [ (L.Rel name (map L.Var r')),
+				(drsConsToLF ( \rs'' -> cs) [r]) ]
+		(DRS _ [Neg d]) -> (\rs' -> L.Neg (drsToLF (\rs'' -> d) rs') ) ts
+	_ -> undefined
 
 drsConsToLF :: ([DRSRef] -> [DRSCon]) -> ([DRSRef] -> L.LF)
 drsConsToLF uc rs = case (uc rs) of
@@ -27,8 +33,9 @@ drsConsToLF uc rs = case (uc rs) of
 	[Rel (DRSRel name) rs'] -> case rs' of
 		[r] -> L.Exists (\t -> L.Rel name [t])
 		_ -> L.Rel name (map L.Var rs')
-	[Neg d] -> L.Neg (drsToLF (\rs' -> d) rs)
-	[Prop p d] -> L.Conj [ (L.Rel (drsRefToDRSVar p) [head ts]), (drsToLF (\rs' -> d) rs) ] where ts = map L.Var rs
+	[Neg d] -> L.Neg (drsToLF (\rs' -> d) ts)
+	[Prop p d] -> L.Conj [ (L.Rel (drsRefToDRSVar p) [head ts]), (drsToLF (\rs' -> d) ts) ]
 	(c:cs) -> L.Conj [ (drsConsToLF (\rs'' -> [c]) rs), (drsConsToLF (\rs'' -> cs) rs) ]
+	where ts = map L.Var rs
 
 -- vim: set ts=2 sts=2 sw=2 noet:
