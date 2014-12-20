1f57fa3144b831bd7ce34f803adddc32943ce553
Dr Bean <drbean@localhost.localdomain>
2014-12-20 11:59:36 +0800

    isRel:DRSCon-Bool.refs:DRSCon-[DRSRef].rel:DRSCon-String.replace:DResRef-Term.drsToLF_case_all_isRel_cs

--- Translate.hs
+++ Translate.hs
@@ -14,11 +14,32 @@ term2ref :: L.Term -> DRSRef
 term2ref (L.Var r) = r
 term2ref _ = undefined
 
+isRel :: DRSCon -> Bool
+isRel (Rel _ _) = True
+isRel _ = False
+
+refs :: DRSCon -> [DRSRef]
+refs (Rel _ rs) = rs
+refs _ = undefined
+
+rel :: DRSCon -> String
+rel (Rel (DRSRel r) _) = r
+rel _ = undefined
+
+replace :: DRSRef -> L.Term
+replace (DRSRef "r1") = L.Var (DRSRef "e1")
+replace (DRSRef "r2") = L.Var (DRSRef "e2")
+replace (DRSRef "r3") = L.Var (DRSRef "e3")
+replace (DRSRef "r4") = L.Var (DRSRef "e4")
+
 drsToLF :: DRSUnresolved -> ([L.Term] -> L.LF)
 drsToLF ud ts = case (ud rs) of
 	(LambdaDRS _) -> error "infelicitous FOL formula"
 	(Merge _ _) -> error "infelicitous FOL formula"
 	(DRS _ []) -> (\t -> L.Top ) ts
+	(DRS _ cs) -> case (all isRel cs) of
+			True -> L.Conj [ (L.Rel (rel c) [replace r | r <- refs c]) | c <- cs]
+			_ -> undefined
 	(DRS _ (Rel (DRSRel name) rs' : cs)) -> case rs' of
 		[r] -> L.Exists (\t -> L.Conj [ (L.Rel name [t]) ,
 				(drsConsToLF ( \rs'' -> cs) (tail rs)) ]
