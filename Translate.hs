6387d58f1cdb85e687e0b30855068c2370acbeb7
Dr Bean <drbean@localhost.localdomain>
2014-12-21 14:27:45 +0800

    replace^ref2term

--- Translate.hs
+++ Translate.hs
@@ -35,13 +35,13 @@ rel :: DRSCon -> String
 rel (Rel (DRSRel r) _) = r
 rel _ = undefined
 
-replace :: [L.Term] -> DRSRef -> L.Term
-replace ts (DRSRef "r1") = ts !! 0
-replace ts (DRSRef "r2") = ts !! 1
-replace ts (DRSRef "r3") = ts !! 2
-replace ts (DRSRef "r4") = ts !! 3
-replace ts (DRSRef p) | isSuffixOf "_prop" p = ts !! 4
-replace ts _ = undefined
+ref2term :: [L.Term] -> DRSRef -> L.Term
+ref2term ts (DRSRef "r1") = ts !! 0
+ref2term ts (DRSRef "r2") = ts !! 1
+ref2term ts (DRSRef "r3") = ts !! 2
+ref2term ts (DRSRef "r4") = ts !! 3
+ref2term ts (DRSRef p) | isSuffixOf "_prop" p = ts !! 4
+ref2term ts _ = undefined
 
 singleton :: [a] -> Bool
 singleton [x]	= True
@@ -53,13 +53,13 @@ drsToLF ud ts
 	| (Merge _ _) <- ud rs = error "infelicitous FOL formula"
 	| (DRS _ []) <- ud rs = (\t -> L.Top ) ts
 	| (DRS _ cs) <- ud rs
-		, all isRel cs = L.Conj [ (L.Rel (rel c) [replace ts r | r <- refs c]) | c <- cs]
+		, all isRel cs = L.Conj [ (L.Rel (rel c) [ref2term ts r | r <- refs c]) | c <- cs]
 	| (DRS _ (Rel (DRSRel name) rs' : cs)) <- ud rs
 		, (singleton rs') = L.Exists (\t -> L.Conj [ (L.Rel name [t]) ,
 				(drsConsToLF ( \rs'' -> cs) (tail rs)) ]
 			 )
 	| (DRS _ (Rel (DRSRel name) rs' : cs)) <- ud rs
-		= L.Conj [ (L.Rel name (map (replace ts) rs')),
+		= L.Conj [ (L.Rel name (map (ref2term ts) rs')),
 			(drsConsToLF ( \rs'' -> cs) rs') ]
 	| (DRS _ [Neg d]) <- ud rs = (\rs' -> L.Neg (drsToLF (\rs'' -> d) rs') ) ts
 	where rs = map (term2ref drsRefs) ts
@@ -69,10 +69,10 @@ drsConsToLF uc rs = case (uc rs) of
 	[] -> L.Top
 	[Rel (DRSRel name) rs'] -> case rs' of
 		[r] -> L.Exists (\t -> L.Rel name [t])
-		_ -> L.Rel name (map (replace ts) rs')
+		_ -> L.Rel name (map (ref2term ts) rs')
 	[Neg d] -> L.Neg (drsToLF (\rs' -> d) ts)
 	[Prop p d] -> L.Conj [ (L.Rel (drsRefToDRSVar p) [head ts]), (drsToLF (\rs' -> d) ts) ]
 	(c:cs) -> L.Conj [ (drsConsToLF (\rs'' -> [c]) rs), (drsConsToLF (\rs'' -> cs) rs) ]
-	where ts = map (replace ts) rs
+	where ts = map (ref2term ts) rs
 
 -- vim: set ts=2 sts=2 sw=2 noet:
