5d023f7a81ba7f4c652a407eb222a698adfccad8
Dr Bean <drbean@localhost.localdomain>
2015-02-28 14:14:05 +0800

    drsRefs:p1,p2.term2ref,ref2term.drsToLF.GCausative.GLook_bad.Props

--- Translate.hs
+++ Translate.hs
@@ -15,9 +15,9 @@ import qualified LogicalForm as L
 type DRSUnresolved = [DRSRef] -> DRS
 
 drsRefs = [ DRSRef "r1", DRSRef "r2", DRSRef "r3", DRSRef "r4",
-	DRSRef "p", DRSRef "r1"]
+	DRSRef "p", DRSRef "p1", DRSRef "p2", DRSRef "r1"]
 
-xyzwp = [L.Var "e1", L.Var "e2", L.Var "e3", L.Var "e4", L.Var "p"]
+xyzwp = [L.Var "e1", L.Var "e2", L.Var "e3", L.Var "e4", L.Var "p", L.Var "p1", L.Var "p2"]
 var_e = L.Var "e"
 
 term2ref :: [DRSRef] -> L.Term -> DRSRef
@@ -26,7 +26,9 @@ term2ref rs (L.Var "e2") = rs !! 1
 term2ref rs (L.Var "e3") = rs !! 2
 term2ref rs (L.Var "e4") = rs !! 3
 term2ref rs (L.Var "p") = rs !! 4
-term2ref rs (L.Var "e") = rs !! 5
+term2ref rs (L.Var "p1") = rs !! 5
+term2ref rs (L.Var "p2") = rs !! 6
+term2ref rs (L.Var "e") = rs !! 7
 term2ref rs x = error ("term2ref " ++ (show x) ++ ": undefined")
 
 isRel :: DRSCon -> Bool
@@ -47,6 +49,8 @@ ref2term ts (DRSRef "r2") = ts !! 1
 ref2term ts (DRSRef "r3") = ts !! 2
 ref2term ts (DRSRef "r4") = ts !! 3
 ref2term ts (DRSRef "p") = ts !! 4
+ref2term ts (DRSRef "p1") = ts !! 5
+ref2term ts (DRSRef "p2") = ts !! 6
 ref2term ts (DRSRef p) | isSuffixOf "_prop" p = ts !! 4
 
 singleton :: [a] -> Bool
@@ -61,6 +65,17 @@ drsToLF :: DRS -> L.LF
 drsToLF (LambdaDRS _) = error "infelicitous FOL formula"
 drsToLF (Merge _ _) = error "infelicitous FOL formula"
 drsToLF (DRS _ []) = L.Top
+
+drsToLF (DRS rl (Rel make [e1,p1] : Prop q1 (DRS _ (Rel posit [e2,p2] : 
+	[Prop q2 (DRS _ [cond] )])): cs))
+	| p1 == q1
+	, p2 == q2
+	= L.Exists (ref2term xyzwp e1) (L.Conj [
+		L.Exists (ref2term xyzwp e2) (L.Conj [
+		L.Rel (drsRelToString make) (map (ref2term xyzwp) [e1,p1])
+		, L.Rel (rel cond) (map (ref2term xyzwp) (p1 : refs cond))
+		, (drsToLF (DRS rl cs)) ]) ])
+
 drsToLF (DRS rl (Rel posit [e,p]: Prop q (DRS _ [cond]): cs))
 	| p == q
 	= L.Exists (ref2term xyzwp p) (L.Conj [
