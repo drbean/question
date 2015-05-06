f61789bed2d65cb117ea90c06b66a47b5ab0f325
Dr Bean <drbean@localhost.localdomain>
2015-05-06 13:13:39 +0800

    Siar^Clay

--- Transfer.hs
+++ Transfer.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Siar
+import Clay
 import LogicalForm hiding ((==))
 import Evaluation
 
@@ -20,7 +20,7 @@ import System.Environment.FindBin
 main :: IO ()
 main = do
 	path <- getProgPath
-	gr <- readPGF ( path ++ "/Siar.pgf" )
+	gr <- readPGF ( path ++ "/Clay.pgf" )
 	hClose stderr
 	hDuplicateTo stdout stderr
 	s <- getLine
