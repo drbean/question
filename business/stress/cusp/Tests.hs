0afa9b0a56a5e467df3ccbf0c1facbfee46c0613
Dr Bean <drbean@localhost.localdomain>
2016-04-02 17:18:57 +0800

    modern_Tests_handler_from_smallwood

--- business/stress/cusp/Tests.hs
+++ business/stress/cusp/Tests.hs
@@ -1,14 +1,62 @@
 module Tests where
 
-import Handler
+import Control.Monad
+import Data.Maybe
 
-all_tests =
-		student_tag_tests ++ student_yn_tests ++ student_tests_wh ++
-		test_text ++ comp_test1 ++ comp_test2 ++ tag_test ++ neg_tag_test ++
-		test_possessives ++ haves ++ intransitives ++ transitives ++ 
-		ditransitive_tests ++ relclauses ++ relppR_test
-		-- ++ wh_questions ++ comp_wh_questions
-		-- ++ ungrammatical
+import Data.DRS
+
+import PGF
+import Cusp
+import Representation
+import Evaluation
+import Model
+import WordsCharacters
+
+-- handler gr core tests = putStr $ unlines $ map (\(x,y) -> x++show y) $ zip (map (++"\t") tests ) ( map (\string -> map (\x -> core ( x) ) (parse gr (mkCId "DicksonEng") (startCat gr) string)) tests )
+
+-- import System.Environment.FindBin
+
+ans tests = do
+  gr	<- readPGF ( "./Cusp.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ls = map (map ( (linear gr) <=< transform ) ) ps
+  let zs = zip (map (++"\t") tests) ls
+  putStrLn (unlines (map (\(x,y) -> x ++ (show $ unwords (map displayResult y))) zs) )
+
+displayResult = fromMaybe "Nothing"
+
+trans tests = do
+  gr	<- readPGF ( "./Cusp.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ls = map id ps
+  let zs = zip (map (++"\t") tests) (map (map (showExpr []) ) ps)
+  putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+reps tests = do
+  gr	<- readPGF ( "./Cusp.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ts = map (map (\x -> (((unmaybe . rep) x) (term2ref drsRefs var_e) ))) ps
+  let zs = zip (map (++"\t") tests) ts
+  putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+lf tests = do
+	gr	<- readPGF ( "./Cusp.pgf" )
+	let ss = map (chomp . lc_first) tests
+	let ps = map ( parses gr ) ss
+	let ts = map (map (\p -> drsToLF (((unmaybe . rep) p) (DRSRef "r1"))) ) ps
+	let zs = zip (map (++"\t") tests) ts
+	putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+fol tests = do
+	gr	<- readPGF ( "./Cusp.pgf" )
+	let ss = map (chomp . lc_first) tests
+	let ps = map ( parses gr ) ss
+	let ts = map (map (\p -> drsToFOL ( (unmaybe . rep) p (term2ref drsRefs var_e) ) ) ) ps
+	let zs = zip (map (++"\t") tests) ts
+	putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
 
 dic_test = [
 
