1d792f8322016270f6db97196d568fd47a3bc40c
Dr Bean <drbean@localhost.localdomain>
2016-04-09 10:20:35 +0800

    previous=cusp

---
+++ business/managing/motivating/Tests.hs
@@ -0,0 +1,64 @@
+module Tests where
+
+import Control.Monad
+import Data.Maybe
+
+import Data.DRS
+
+import PGF
+import Cusp
+import Representation
+import Evaluation
+import Model
+import WordsCharacters
+
+-- handler gr core tests = putStr $ unlines $ map (\(x,y) -> x++show y) $ zip (map (++"\t") tests ) ( map (\string -> map (\x -> core ( x) ) (parse gr (mkCId "DicksonEng") (startCat gr) string)) tests )
+
+-- import System.Environment.FindBin
+
+ans tests = do
+  gr	<- readPGF ( "./Cusp.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ls = map (map ( (linear gr) <=< transform ) ) ps
+  let zs = zip (map (++"\t") tests) ls
+  putStrLn (unlines (map (\(x,y) -> x ++ (show $ unwords (map displayResult y))) zs) )
+
+displayResult = fromMaybe "Nothing"
+
+trans tests = do
+  gr	<- readPGF ( "./Cusp.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ls = map id ps
+  let zs = zip (map (++"\t") tests) (map (map (showExpr []) ) ps)
+  putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+reps tests = do
+  gr	<- readPGF ( "./Cusp.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ts = map (map (\x -> (((unmaybe . rep) x) (term2ref drsRefs var_e) ))) ps
+  let zs = zip (map (++"\t") tests) ts
+  putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+lf tests = do
+	gr	<- readPGF ( "./Cusp.pgf" )
+	let ss = map (chomp . lc_first) tests
+	let ps = map ( parses gr ) ss
+	let ts = map (map (\p -> drsToLF (((unmaybe . rep) p) (DRSRef "r1"))) ) ps
+	let zs = zip (map (++"\t") tests) ts
+	putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+fol tests = do
+	gr	<- readPGF ( "./Cusp.pgf" )
+	let ss = map (chomp . lc_first) tests
+	let ps = map ( parses gr ) ss
+	let ts = map (map (\p -> drsToFOL ( (unmaybe . rep) p (term2ref drsRefs var_e) ) ) ) ps
+	let zs = zip (map (++"\t") tests) ts
+	putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+dic_test = [
+  ]
+
+-- vim: set ts=2 sts=2 sw=2 noet:
