5846c1565ebdc06a4e5f191cba312b93fb128b9c
Dr Bean <drbean@localhost.localdomain>
2016-04-09 10:25:38 +0800

    dic_test

--- business/managing/motivating/Tests.hs
+++ business/managing/motivating/Tests.hs
@@ -59,6 +59,18 @@ fol tests = do
 	putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
 
 dic_test = [
+  "Managers need to understand what motivates people."
+	, "There are some things that motivate everyone."
+	, "People care about their families,"
+	, "Everybody wants to make a difference."
+	, "Everybody wants to be respected."
+  , "More difficult is understanding how everyone is unique and different."
+	, "Everyone is different to the way that you as a manager think."
+	, "The secret is to try to know what the world looks for people through their eyes."
+	, "Managers need to understand what the world looks like through the eyes of another person."
+	, "In order to look at the world through the eyes of another person, managers need to be good at asking questions and listening to people."
+	, "Managers need to ask not just any question, but questions that really tell them about what a person's drivers, or unique motivators are."
+	, "Managers need to ask questions to find out what other people are concerned about in the situation that they are both in."
   ]
 
 -- vim: set ts=2 sts=2 sw=2 noet:
