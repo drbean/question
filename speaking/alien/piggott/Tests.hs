effbd3fc23aa585322a041c1a56bbe8d9184c31a
Dr Bean <drbean@localhost.localdomain>
2016-04-14 09:30:28 +0800

    dot_boilerplate_working

---
+++ speaking/alien/piggott/Tests.hs
@@ -0,0 +1,78 @@
+module Tests where
+
+import Control.Monad
+import Data.Maybe
+
+import Data.DRS
+
+import PGF
+import Motivating
+import Representation
+import Evaluation
+import Model
+import WordsCharacters
+
+-- handler gr core tests = putStr $ unlines $ map (\(x,y) -> x++show y) $ zip (map (++"\t") tests ) ( map (\string -> map (\x -> core ( x) ) (parse gr (mkCId "DicksonEng") (startCat gr) string)) tests )
+
+-- import System.Environment.FindBin
+
+ans tests = do
+  gr	<- readPGF ( "./Motivating.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ls = map (map ( (linear gr) <=< transform ) ) ps
+  let zs = zip (map (++"\t") tests) ls
+  putStrLn (unlines (map (\(x,y) -> x ++ (show $ unwords (map displayResult y))) zs) )
+
+displayResult = fromMaybe "Nothing"
+
+trans tests = do
+  gr	<- readPGF ( "./Motivating.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ls = map id ps
+  let zs = zip (map (++"\t") tests) (map (map (showExpr []) ) ps)
+  putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+reps tests = do
+  gr	<- readPGF ( "./Motivating.pgf" )
+  let ss = map (chomp . lc_first) tests
+  let ps = map ( parses gr ) ss
+  let ts = map (map (\x -> (((unmaybe . rep) x) (term2ref drsRefs var_e) ))) ps
+  let zs = zip (map (++"\t") tests) ts
+  putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+lf tests = do
+	gr	<- readPGF ( "./Motivating.pgf" )
+	let ss = map (chomp . lc_first) tests
+	let ps = map ( parses gr ) ss
+	let ts = map (map (\p -> drsToLF (((unmaybe . rep) p) (DRSRef "r1"))) ) ps
+	let zs = zip (map (++"\t") tests) ts
+	putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+fol tests = do
+	gr	<- readPGF ( "./Motivating.pgf" )
+	let ss = map (chomp . lc_first) tests
+	let ps = map ( parses gr ) ss
+	let ts = map (map (\p -> drsToFOL ( (unmaybe . rep) p (term2ref drsRefs var_e) ) ) ) ps
+	let zs = zip (map (++"\t") tests) ts
+	putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+dic_test = [
+
+  "Managers need to understand what motivates people."
+	, "There are some things that motivate everyone."
+	, "People care about their families,"
+	, "Everybody wants to make a difference."
+	, "Everybody wants to be respected."
+  , "Understanding how everyone is unique and different is more difficult."
+	, "Everyone is different to the way that you as a manager think."
+	, "The secret is to try to know what the world looks for people through their eyes."
+	, "Managers need to understand what the world looks like through the eyes of another person."
+	, "Managers need to be good at asking questions and listening to people, in order to look at the world through the eyes of another person."
+	, "Managers need to ask not just any question, but questions that really tell them about what a person's drivers, or unique motivators are."
+	, "Managers need to ask questions to find out what other people are concerned about in the situation that they are both in."
+
+  ]
+
+-- vim: set ts=2 sts=2 sw=2 noet:
---
+++ speaking/alien/piggott/WordsCharacters.hs
@@ -0,0 +1,260 @@
+module WordsCharacters where
+
+import qualified Data.Map as Map
+
+gfWords = Map.fromList [
+	("A",a)
+	, ("ADV",adv)
+	, ("Aux",aux)
+	, ("CONJ",conj)
+	, ("Det",det)
+	, ("N",n)
+	, ("PN",pn)
+	, ("Pron",pron)
+	, ("Prep",prep)
+	, ("Rel",rel)
+	, ("Tag",tag)
+	, ("V",v) ]
+
+wordlist = concat ( map (gfWords Map.!) (Map.keys gfWords) )
+
+posMap = Map.fromList [
+	("A","Adjective")
+	, ("ADV","Adverb")
+	, ("Aux","Auxiliary")
+	, ("CONJ","Conjunction")
+	, ("Det","Determiner")
+	, ("N","Noun")
+	, ("PN","Proper Noun")
+	, ("Pron","Pronoun")
+	, ("Prep","Preposition")
+	, ("Rel","Relative Pronoun")
+	, ("Tag","Question Tag")
+	, ("V","Verb")
+	]
+
+a = [
+
+	"concerned"
+	, "different"
+	, "difficult"
+	, "good"
+	, "respected"
+	, "unique"
+	]
+
+adv = [
+
+	"just"
+	, "out"
+	, "really"
+	, "more"
+	]
+
+aux = [
+	"doesn't"
+	, "don't"
+	, "does"
+	, "do"
+	, "is"
+	, "are"
+	, "isn't"
+	, "aren't"
+	, "would"
+	, "should"
+	]
+	
+
+conj = [
+	"and"
+	, "because"
+	, "but"
+	, "if"
+	, "or"
+	]
+
+
+det = [
+	"'s"
+	, "0, _ or zero"
+	, "a"
+	, "an"
+	, "no"
+	, "some"
+	, "the"
+	, "another"
+	, "any"
+	, "both"
+	, "other"
+	, "their"
+
+  ]
+
+n = [
+
+	"everybody"
+	, "everyone"
+	, "difference"
+	, "driver"
+	, "eye"
+	, "family"
+	, "manager"
+	, "motivator"
+	, "people"
+	, "person"
+	, "question"
+	, "secret"
+	, "situation"
+	, "thing"
+	, "way"
+	, "world"
+	]
+
+pn = [
+
+	]
+
+pron = [
+	"who"
+	, "she"
+	, "her"
+	, "he"
+	, "his"
+	, "that"
+	, "there"
+	, "they"
+	, "you"
+	]
+
+prep = [
+
+	"about"
+	, "as"
+	, "at"
+	, "for"
+	, "in"
+	, "like"
+	, "of"
+	, "in order to"
+	, "through"
+	, "to"
+	]
+
+rel = [
+	"that"
+	, "who"
+	, "how"
+	, "what"
+	]
+
+tag = [
+	"doesn't he"
+	, "doesn't she"
+	, "doesn't it"
+	, "don't they"
+	, "does he"
+	, "does she"
+	, "does it"
+	, "do they"
+	, "isn't he"
+	, "isn't she"
+	, "isn't it"
+	, "aren't they"
+	, "is he"
+	, "is she"
+	, "is it"
+	, "are they"
+	]
+
+v = [
+
+	"ask"
+	, "care"
+	, "find"
+	, "know"
+	, "listen"
+	, "look"
+	, "make"
+	, "motivate"
+	, "need"
+	, "tell"
+	, "think"
+	, "try"
+	, "understand"
+	, "want"
+	]
+
+{-
+
+
+about	: Prep;
+and	: Conj;
+another	: Det;
+any	: Det;
+as	: Prep;
+ask	: V2;
+at	: Prep;
+both	: Det;
+but	: Conj;
+care	: V2;
+concerned	: A;
+difference	: CN;
+different	: A;
+difficult	: A;
+driver	: CN;
+everybody	: N;
+everyone	: N;
+eye	: CN;
+family	: CN;
+find	: V2;
+for	: Prep;
+good	: A;
+how	: Rel;
+in	: Prep;
+just	: Adv;
+know	: V2;
+like	: Prep;
+listen	: V2;
+look	: V2;
+make	: V2;
+manager	: CN;
+more	: Adv;
+motivate	: V2;
+motivator	: CN;
+need	: VV;
+not
+of	: Prep;
+or	: Conj;
+in order to	: Prep;
+other	: Det;
+out	: Adv;
+people	: CN;
+person	: CN;
+question	: CN;
+really	: Adv;
+respected	: A;
+secret	: CN;
+situation	: CN;
+some	: Det;
+tell	: V3;
+that	: Pron;
+their	: Det;
+them	: Pron;
+there	: Pron;
+they	: Pron;
+thing	: CN;
+think	: V;
+through	: Prep;
+to	: Prep;
+try	: VV;
+understand	: V2;
+unique	: A;
+want	: VV;
+way	: CN;
+what	: Rel;
+world	: CN;
+you	: Pron;
+
+-}
+
+-- vim: set ts=2 sts=2 sw=2 noet:
