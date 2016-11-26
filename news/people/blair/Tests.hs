64a44ca8b1e54dd44db68f8ae5214069a5c0c3e1
Dr Bean <drbean@localhost.localdomain>
2016-11-26 16:32:12 +0800

    space-separated_apposition_comma

--- news/people/blair/Tests.hs
+++ news/people/blair/Tests.hs
@@ -97,8 +97,8 @@ fol tests = do
 
 dic_test = [
 
-	"Blair works at the New York Times as an intern with another intern, Hernandez."
-	, "Hernandez leaves the Times to work at a newspaper in Texas, her home state."
+	"Blair works at the New York Times as an intern with another intern , Hernandez."
+	, "Hernandez leaves the Times to work at a newspaper in Texas , her home state."
 	, "In Texas, Hernandez interviews the mother of a soldier missing in Iraq."
 	, "Blair copies information from Hernandez' story for his own story in the Times."
 	, "Hernandez' tells Raines, Blair's editor, who fires Blair." 
