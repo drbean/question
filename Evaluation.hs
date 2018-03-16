d6fecd85c2d45e988275745451186c50c5391102
Dr Bean <drbean@sac.nuu.edu.tw>
2018-03-16 12:06:45 +0800

    U+FF1F ？ FULLWIDTH QUESTION MARK (HTML &#65311;

--- Evaluation.hs
+++ Evaluation.hs
@@ -173,7 +173,7 @@ chomp (' ': 'i': 't': '\'': 's': ' ': xs)	= " it is " ++ chomp xs
 chomp (' ': ',': ' ': xs) = " , " ++ chomp xs
 chomp ('1': ',': '0': xs) = "1,0" ++ chomp xs
 chomp ('1': ',': '8': xs) = "1,8" ++ chomp xs
-chomp (x:xs) | x `elem` ".,?" = chomp xs
+chomp (x:xs) | x `elem` ".,?？" = chomp xs
             | otherwise      =     x:chomp xs
 
 
